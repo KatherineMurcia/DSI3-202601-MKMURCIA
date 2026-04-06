816f7166b8c70a5929d7522e7f0cac703be47069
KATHERINE MURCIA <mkmurcia@unbosque.edu.co>
2026-04-06 18:01:28 -0500

    feat(feature/atomic_design): agregada ruta auth/register

--- atomic-login/src/app/app.routes.ts
+++ atomic-login/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page';
+import { RegisterPageComponent } from './pages/register-page/register-page';
 
 export const routes: Routes = [
   { path: 'auth/login', component: LoginPageComponent },
+  { path: 'auth/register', component: RegisterPageComponent },
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' }
 ];
