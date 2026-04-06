f32feb1140690ae191b3060cfa1a520e193a7393
KATHERINE MURCIA <mkmurcia@unbosque.edu.co>
2026-04-06 16:55:55 -0500

    feat(feature/atomic_design): configuracion rutas y app root

--- atomic-login/src/app/app.routes.ts
+++ atomic-login/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth/login', component: LoginPageComponent },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' }
+];
