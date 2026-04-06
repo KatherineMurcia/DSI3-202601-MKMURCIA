640c3a4c5528fd7b6fab4caf571fe5ed7c6ed760
KATHERINE MURCIA <mkmurcia@unbosque.edu.co>
2026-04-06 16:55:24 -0500

    feat(feature/atomic_design): implementacion organismo login

--- atomic-login/src/app/components/organisms/login-form/login-form.ts
+++ atomic-login/src/app/components/organisms/login-form/login-form.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { LoginHeaderComponent } from '../../molecules/login-header/login-header';
+import { CredentialsFormComponent } from '../../molecules/credentials-form/credentials-form';
+import { FormActionsComponent } from '../../molecules/form-actions/form-actions';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  standalone: true,
+  imports: [LoginHeaderComponent, CredentialsFormComponent, FormActionsComponent],
   templateUrl: './login-form.html',
-  styleUrl: './login-form.css',
+  styleUrl: './login-form.css'
 })
-export class LoginForm {}
+export class LoginFormComponent {}
