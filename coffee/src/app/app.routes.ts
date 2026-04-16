2b44526bbad9308cd91c02f192d09c97fd588bff
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-04-15 21:28:52 -0500

    feat(feature/cafeteria_angular): implementación de lógica principal y configuración de rutas

--- coffee/src/app/app.routes.ts
+++ coffee/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CoffeeScreen } from './screens/coffee-screen/coffee-screen';
+import { LoginScreen } from './screens/login-screen/login-screen';
+import { DashboardScreen } from './screens/dashboard-screen/dashboard-screen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CoffeeScreen },
+  { path: 'login', component: LoginScreen },
+  { path: 'dashboard', component: DashboardScreen },
+  { path: '**', redirectTo: '' },
+];
--- coffee/src/app/app.ts
+++ coffee/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoffeeHeader } from './components/organisms/coffee-header/coffee-header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoffeeHeader],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
