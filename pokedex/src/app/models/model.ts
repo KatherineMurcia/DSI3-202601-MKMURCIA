72b4f773a68fd2606729da0e1b4dfa5bbbf55b12
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-04-17 23:42:03 -0500

    feat(feature/pokedex): agrega modelos de datos para Pokémon (DTOs para lista y detalle de la PokéAPI)

---
+++ pokedex/src/app/models/model.ts
@@ -0,0 +1,32 @@
+export interface PokemonsDto {
+  count: number;
+  next: String;
+  previous: String | null;
+  results: PokemonsListDto[];
+}
+
+export interface PokemonsListDto {
+  name: string;
+  url: String;
+}
+export interface PokemonDetailDto {
+  id : number;
+  name: string;
+  type: PokemonDetailListTypeDTO [];
+  sprites: PokemonDetailSpriteDto;
+}
+export interface PokemonDetailListTypeDTO {
+    slot: number;
+    type: PokemonDetailTypeDTO
+  }
+
+export interface PokemonDetailTypeDTO {
+  name: string;
+  url: String;
+}
+
+export interface PokemonDetailSpriteDto {
+  front_default: String;
+  front_shiny: String;
+}
+
