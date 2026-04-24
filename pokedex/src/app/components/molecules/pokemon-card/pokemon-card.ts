38363b7e43baa1c83f58e0de143a32bb97d4b868
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-04-24 18:50:49 -0500

    refactor(feature/pokedex): usar átomo badge en pokemon-card

--- pokedex/src/app/components/molecules/pokemon-card/pokemon-card.ts
+++ pokedex/src/app/components/molecules/pokemon-card/pokemon-card.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonDetailDto } from '../../../models/model';
 import { PokemonImage } from '../../atoms/pokemon-image/pokemon-image';
+import { Badge } from '../../atoms/badge/badge';
 
 @Component({
   selector: 'app-pokemon-card',
-  imports: [CommonModule, PokemonImage],
+  imports: [CommonModule, PokemonImage, Badge],
   templateUrl: './pokemon-card.html',
   styleUrl: './pokemon-card.css',
 })
