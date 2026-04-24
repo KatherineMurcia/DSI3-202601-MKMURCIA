4010feeb678ede2d7de8ec8b53c0517a5b6ceede
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-04-24 18:50:51 -0500

    refactor(feature/pokedex): usar átomo badge y agregar borde en pokemon-detail

--- pokedex/src/app/components/organisms/pokemon-detail/pokemon-detail.ts
+++ pokedex/src/app/components/organisms/pokemon-detail/pokemon-detail.ts
@@ -2,10 +2,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonDetailDto } from '../../../models/model';
 import { SearchBar } from '../../molecules/search-bar/search-bar';
+import { Badge } from '../../atoms/badge/badge';
 
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [CommonModule, SearchBar],
+  imports: [CommonModule, SearchBar, Badge],
   templateUrl: './pokemon-detail.html',
   styleUrl: './pokemon-detail.css',
 })
