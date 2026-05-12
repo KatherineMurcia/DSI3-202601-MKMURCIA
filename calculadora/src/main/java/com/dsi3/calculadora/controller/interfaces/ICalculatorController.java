bfcf6ab6fd8ff597158d2c6073c553f5bed68b0b
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-11 19:35:22 -0500

    feat(feature/calculadora): agregar interfaz ICalculatorController

---
+++ calculadora/src/main/java/com/dsi3/calculadora/controller/interfaces/ICalculatorController.java
@@ -0,0 +1,26 @@
+package com.dsi3.calculadora.controller.interfaces;
+
+
+import com.dsi3.calculadora.model.dto.CalculadoraRequestDTO;
+import com.dsi3.calculadora.model.dto.CalculadoraResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/calculadora/operaciones")
+public interface ICalculatorController {
+
+    @PostMapping("/suma")
+    ResponseEntity<CalculadoraResponseDTO> suma(@RequestBody CalculadoraRequestDTO request);
+
+    @PostMapping("/resta")
+    ResponseEntity<CalculadoraResponseDTO> resta(@RequestBody CalculadoraRequestDTO request);
+
+    @PostMapping("/multiplicacion")
+    ResponseEntity<CalculadoraResponseDTO> multiplicacion(@RequestBody CalculadoraRequestDTO request);
+    
+    @PostMapping("/division")
+    ResponseEntity<CalculadoraResponseDTO> division(@RequestBody CalculadoraRequestDTO request);
+
+}
