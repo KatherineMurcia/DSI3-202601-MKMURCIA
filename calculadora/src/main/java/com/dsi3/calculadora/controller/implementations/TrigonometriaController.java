5146bbf94e03d5c3ebce3b3eb8cd8279c17453ca
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-11 21:35:11 -0500

    feat(feature/calculadora): implementar TrigonometriaController

---
+++ calculadora/src/main/java/com/dsi3/calculadora/controller/implementations/TrigonometriaController.java
@@ -0,0 +1,28 @@
+package com.dsi3.calculadora.controller.implementations;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dsi3.calculadora.controller.interfaces.ITrigonometriaController;
+import com.dsi3.calculadora.model.dto.TrigonometriaRequestDTO;
+import com.dsi3.calculadora.model.dto.TrigonometriaResponseDTO;
+import com.dsi3.calculadora.service.interfaces.ITrigonometriaService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class TrigonometriaController implements ITrigonometriaController {
+
+    private final ITrigonometriaService trigonometriaService;
+
+    @Override
+    public ResponseEntity<TrigonometriaResponseDTO> calcularSeno(TrigonometriaRequestDTO request) {
+        return trigonometriaService.calcularSeno(request);
+    }
+
+    @Override
+    public ResponseEntity<TrigonometriaResponseDTO> calcularCoseno(TrigonometriaRequestDTO request) {
+        return trigonometriaService.calcularCoseno(request);
+    }
+}
