94b2b1fc7e70fb6dac63af3a9c28a81f27ac75d5
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-11 21:49:08 -0500

    feat(feature/calculadora): agregar metodos de tangente, cosecante, secante y cotangente en ITrigonometriaService

--- calculadora/src/main/java/com/dsi3/calculadora/service/interfaces/ITrigonometriaService.java
+++ calculadora/src/main/java/com/dsi3/calculadora/service/interfaces/ITrigonometriaService.java
@@ -7,4 +7,8 @@ import org.springframework.http.ResponseEntity;
 public interface ITrigonometriaService {
     ResponseEntity<TrigonometriaResponseDTO> calcularSeno(TrigonometriaRequestDTO request);
     ResponseEntity<TrigonometriaResponseDTO> calcularCoseno(TrigonometriaRequestDTO request);
+    ResponseEntity<TrigonometriaResponseDTO> calcularTangente(TrigonometriaRequestDTO request);
+    ResponseEntity<TrigonometriaResponseDTO> calcularCosecante(TrigonometriaRequestDTO request);
+    ResponseEntity<TrigonometriaResponseDTO> calcularSecante(TrigonometriaRequestDTO request);
+    ResponseEntity<TrigonometriaResponseDTO> calcularCotangente(TrigonometriaRequestDTO request);
 }
