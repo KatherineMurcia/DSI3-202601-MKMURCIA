b7bdabc75a129ffea2dae4318f23d7a73898ded4
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-11 19:32:19 -0500

    feat: agregar DTO de respuesta para interes compuesto

---
+++ calculadora/src/main/java/com/dsi3/calculadora/model/dto/InteresCompuestoResponseDTO.java
@@ -0,0 +1,16 @@
+package com.dsi3.calculadora.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InteresCompuestoResponseDTO {
+
+    private double capitalFinal;
+
+}
