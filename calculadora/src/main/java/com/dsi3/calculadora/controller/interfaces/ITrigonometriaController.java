74d86a434665a3bb42f0fbda2d3aa2562c5abcb7
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-11 21:35:10 -0500

    feat(feature/calculadora): agregar interfaz ITrigonometriaController

---
+++ calculadora/src/main/java/com/dsi3/calculadora/controller/interfaces/ITrigonometriaController.java
@@ -0,0 +1,18 @@
+package com.dsi3.calculadora.controller.interfaces;
+
+import com.dsi3.calculadora.model.dto.TrigonometriaRequestDTO;
+import com.dsi3.calculadora.model.dto.TrigonometriaResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/calculadora/trigonometria")
+public interface ITrigonometriaController {
+
+    @PostMapping("/seno")
+    ResponseEntity<TrigonometriaResponseDTO> calcularSeno(@RequestBody TrigonometriaRequestDTO request);
+
+    @PostMapping("/coseno")
+    ResponseEntity<TrigonometriaResponseDTO> calcularCoseno(@RequestBody TrigonometriaRequestDTO request);
+}
