26b9ef601c3fad0f7e1013b79508d0c133b6b30b
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-15 21:46:27 -0500

    feat(api-usuarios): agregar metodos CRUD a interfaz de servicio

--- api-usuarios/src/main/java/com/dsi3/api_usuarios/service/interfaces/InterfaceUsuarioService.java
+++ api-usuarios/src/main/java/com/dsi3/api_usuarios/service/interfaces/InterfaceUsuarioService.java
@@ -6,6 +6,10 @@ import com.dsi3.api_usuarios.model.dto.UsuarioRequestDTO;
 import com.dsi3.api_usuarios.model.dto.UsuarioResponseDTO;
 
 public interface InterfaceUsuarioService {
-ResponseEntity<UsuarioResponseDTO>crearUsuarios(UsuarioRequestDTO request);
-ResponseEntity<List<UsuarioResponseDTO>> obtenerUsuarios();
+    ResponseEntity<UsuarioResponseDTO> crearUsuarios(UsuarioRequestDTO request);
+    ResponseEntity<List<UsuarioResponseDTO>> obtenerUsuarios();
+    ResponseEntity<UsuarioResponseDTO> obtenerUsuario(Long id);
+    ResponseEntity<UsuarioResponseDTO> actualizarUsuario(UsuarioRequestDTO request);
+    ResponseEntity<String> eliminarUsuario(Long id);
+    ResponseEntity<UsuarioResponseDTO> login(UsuarioRequestDTO request);
 }
