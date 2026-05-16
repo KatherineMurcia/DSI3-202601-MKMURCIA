af585299eca1b17c59d5dacb8495211bd1f58449
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-15 21:46:31 -0500

    feat(api-usuarios): implementar endpoints CRUD en UsuarioController

--- api-usuarios/src/main/java/com/dsi3/api_usuarios/controller/implementations/UsuarioController.java
+++ api-usuarios/src/main/java/com/dsi3/api_usuarios/controller/implementations/UsuarioController.java
@@ -1,21 +1,18 @@
 package com.dsi3.api_usuarios.controller.implementations;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.dsi3.api_usuarios.controller.Interfaces.InterfaceUsuarioController;
 import com.dsi3.api_usuarios.model.dto.UsuarioRequestDTO;
 import com.dsi3.api_usuarios.model.dto.UsuarioResponseDTO;
 import com.dsi3.api_usuarios.service.interfaces.InterfaceUsuarioService;
-
 import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-public class UsuarioController implements InterfaceUsuarioController{
-    
+public class UsuarioController implements InterfaceUsuarioController {
+
     private final InterfaceUsuarioService usuarioService;
 
     @Override
@@ -26,7 +23,25 @@ public class UsuarioController implements InterfaceUsuarioController{
     @Override
     public ResponseEntity<List<UsuarioResponseDTO>> obtenerUsuarios() {
         return usuarioService.obtenerUsuarios();
-
     }
 
+    @Override
+    public ResponseEntity<UsuarioResponseDTO> obtenerUsuario(Long id) {
+        return usuarioService.obtenerUsuario(id);
+    }
+
+    @Override
+    public ResponseEntity<UsuarioResponseDTO> actualizarUsuario(UsuarioRequestDTO request) {
+        return usuarioService.actualizarUsuario(request);
+    }
+
+    @Override
+    public ResponseEntity<String> eliminarUsuario(Long id) {
+        return usuarioService.eliminarUsuario(id);
+    }
+
+    @Override
+    public ResponseEntity<UsuarioResponseDTO> login(UsuarioRequestDTO request) {
+        return usuarioService.login(request);
+    }
 }
