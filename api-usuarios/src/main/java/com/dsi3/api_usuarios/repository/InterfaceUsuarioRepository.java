0e27eaaae9abfc7b63602b66d9c51a770c3d797d
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-15 21:46:33 -0500

    feat(api-usuarios): actualizar repositorio de usuarios

--- api-usuarios/src/main/java/com/dsi3/api_usuarios/repository/InterfaceUsuarioRepository.java
+++ api-usuarios/src/main/java/com/dsi3/api_usuarios/repository/InterfaceUsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.dsi3.api_usuarios.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dsi3.api_usuarios.model.entity.Usuario;
 
 public interface InterfaceUsuarioRepository extends JpaRepository<Usuario, Long>{
+  Optional<Usuario> findByEmail(String email);
 
 }
