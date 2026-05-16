c8e52d3f011bb9887049ad7541bc3e97ab4b0e57
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-15 21:46:30 -0500

    feat(api-usuarios): agregar endpoints CRUD a interfaz de controlador

--- api-usuarios/src/main/java/com/dsi3/api_usuarios/controller/Interfaces/InterfaceUsuarioController.java
+++ api-usuarios/src/main/java/com/dsi3/api_usuarios/controller/Interfaces/InterfaceUsuarioController.java
@@ -3,8 +3,11 @@ package com.dsi3.api_usuarios.controller.Interfaces;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.dsi3.api_usuarios.model.dto.UsuarioRequestDTO;
@@ -19,5 +22,16 @@ public interface InterfaceUsuarioController {
     @GetMapping()
     ResponseEntity<List<UsuarioResponseDTO>> obtenerUsuarios();
 
+    @PutMapping
+    ResponseEntity<UsuarioResponseDTO> actualizarUsuario(@RequestBody UsuarioRequestDTO request);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<String> eliminarUsuario(@PathVariable Long id);
+
+    @GetMapping("/{id}")
+    ResponseEntity<UsuarioResponseDTO> obtenerUsuario(@PathVariable Long id);
+
+    @PostMapping("/session")
+    ResponseEntity<UsuarioResponseDTO> login(@RequestBody UsuarioRequestDTO request);
 
 }
