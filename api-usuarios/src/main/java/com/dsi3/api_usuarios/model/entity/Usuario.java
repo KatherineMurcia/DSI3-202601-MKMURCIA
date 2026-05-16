59d07c0c805367d742785fbde349fffeb68f60ff
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-15 21:46:21 -0500

    feat(api-usuarios): agregar campos email, password y edad a entidad Usuario

--- api-usuarios/src/main/java/com/dsi3/api_usuarios/model/entity/Usuario.java
+++ api-usuarios/src/main/java/com/dsi3/api_usuarios/model/entity/Usuario.java
@@ -1,18 +1,32 @@
 package com.dsi3.api_usuarios.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Entity 
+@Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private int edad;
+    
 }
