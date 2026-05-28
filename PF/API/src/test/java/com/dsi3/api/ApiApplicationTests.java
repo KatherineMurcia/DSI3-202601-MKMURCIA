5183c409125f521c3fa5b8fdbf906e4dcc5320a5
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-28 17:27:21 -0500

    fix(feature/proyecto_final): se renombra clase de pruebas a ApiApplicationTests

--- PF/API/src/test/java/com/dsi3/api/ApiApplicationTests.java
+++ PF/API/src/test/java/com/dsi3/api/ApiApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiProductosApplicationTests {
+class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
 }
