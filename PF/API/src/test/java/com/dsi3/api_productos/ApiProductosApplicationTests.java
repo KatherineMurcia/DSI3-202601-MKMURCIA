1e6e7648878c52aa010a89dbb819c7b3005ac64a
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-28 16:50:44 -0500

    test(feature/proyecto_final): se agrega test de contexto generado por Spring Initializr

---
+++ PF/API/src/test/java/com/dsi3/api_productos/ApiProductosApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dsi3.api_productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
