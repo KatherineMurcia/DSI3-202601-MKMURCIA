39e84d5bde8ef2773edabe0717aa034a133830ac
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-28 15:36:32 -0500

    feat(feature/proyecto_final): se agrega servicio de dashboard con estadísticas en tiempo real del restaurante

---
+++ PF/API/src/main/java/com/dsi3/api_productos/service/interfaces/InterfaceDashboardService.java
@@ -0,0 +1,8 @@
+package com.dsi3.api_productos.service.interfaces;
+
+import com.dsi3.api_productos.model.dto.DashboardResumenDTO;
+
+public interface InterfaceDashboardService {
+
+    DashboardResumenDTO resumen();
+}
