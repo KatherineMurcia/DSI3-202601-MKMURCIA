bac830aba5375647fdfd124c36abdeea94900a27
KatherineMurcia <mkmurcia@unbosque.edu.co>
2026-05-28 17:20:08 -0500

    fix(feature/proyecto_final): se renombra clase principal a ApiApplication

--- PF/API/src/main/java/com/dsi3/api/ApiApplication.java
+++ PF/API/src/main/java/com/dsi3/api/ApiApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiProductosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiProductosApplication.class, args);
-	}
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
